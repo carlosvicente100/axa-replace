b6668c448e23c91c5bfddcb06ba90e92ed7cd523
Alejandro Consigli <alejandrojose.consigli.externo@axa-assistance.es>
2022-07-28 12:03:09 +0200

    Starting App and Tabs

--- src/App.js
+++ src/App.js
@@ -1,22 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import React from "react";
+import { Tabs, Tab } from "@mui/material";
+import Personal from "./Tabs/Personal";
+import Team from "./Tabs/Team";
+import Company from "./Tabs/Company";
 
 function App() {
+  const [tab, setTab] = useState(0);
+
+  const handleChange = (e, tab) => {
+    setTab(tab);
+  };
+
+  const tabs = ["Company", "Team", "Personal"];
+
+  const TabChildren = () => {
+    let children;
+    switch (tabs[tab]) {
+      case "Company":
+        children = <Company />;
+        break;
+      case "Team":
+        children = <Team />;
+        break;
+      case "Personal":
+        children = <Personal />;
+        break;
+      default:
+        break;
+    }
+    return children;
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="app">
+      <header className="header">
+        <h1 className="title">Replace App</h1>
+        <Tabs
+          value={tab}
+          onChange={handleChange}
+          aria-label="disabled tabs example"
+          classes={{ root: "tabs", scroller: "tabs-scroller" }}
         >
-          Learn React
-        </a>
+          {tabs.map((tab) => (
+            <Tab label={tab} classes={{ root: "tab", selected: "tab-selected" }} />
+          ))}
+        </Tabs>
+        <div className="tabs-children">
+          <TabChildren />
+        </div>
       </header>
     </div>
   );
